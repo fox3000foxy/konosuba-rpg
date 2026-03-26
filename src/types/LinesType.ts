c7b8cfc15587836c423bb6ff0db9b55d3a207b22
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-26 15:13:07 +0100

    removed color property, typed messages and LinesType

--- src/types/LinesType.ts
+++ src/types/LinesType.ts
@@ -1,9 +1,10 @@
+import { MessagesTemplates } from "../classes/Creature";
 import { Lang } from "../enums/Lang";
 
 export type LinesType = {
   [key in Lang]: {
-    youAttackMsgs: string[][];
-    youDefendMsgs: string[][];
-    youHugMsgs: string[][];
+    youAttackMsgs: MessagesTemplates[][];
+    youDefendMsgs: MessagesTemplates[][];
+    youHugMsgs: MessagesTemplates[][];
   };
 };
