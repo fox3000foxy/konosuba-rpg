f646c67cb77b3aa4cdcd48a6ba0b4ebb69a3f444
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-26 14:50:51 +0100

    TODO: commandsUpdater

--- src/commandsUpdater.ts
+++ src/commandsUpdater.ts
@@ -1,17 +1,17 @@
-import { Client } from 'discord.js';
-import fs from 'fs';
+// import { Client } from 'discord.js';
+// import fs from 'fs';
 
-const client = new Client({ intents: [] });
+// const client = new Client({ intents: [] });
 
-if (!process.env.DISCORD_TOKEN) {
-  throw new Error("DISCORD_TOKEN manquant dans les variables d'environnement");
-}
+// if (!process.env.DISCORD_TOKEN) {
+//   throw new Error("DISCORD_TOKEN manquant dans les variables d'environnement");
+// }
 
-client.on('ready', async () => {
-  const commands = JSON.parse(fs.readFileSync(__dirname + '/commands.json', 'utf-8'));
-  await client.application?.commands.set(commands);
-  console.log('Commandes appliquées');
-  client.destroy();
-});
+// client.on('ready', async () => {
+//   const commands = JSON.parse(fs.readFileSync(__dirname + '/commands.json', 'utf-8'));
+//   await client.application?.commands.set(commands);
+//   console.log('Commandes appliquées');
+//   client.destroy();
+// });
 
-client.login(process.env.DISCORD_TOKEN);
+// client.login(process.env.DISCORD_TOKEN);
