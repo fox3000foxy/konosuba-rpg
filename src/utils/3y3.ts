b9b5bc96962591114974b7c115c1e7ab9290f82e
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-26 18:18:51 +0100

    added 3y3 utility

---
+++ src/utils/3y3.ts
@@ -0,0 +1,37 @@
+function encode3y3(str: { length: number; codePointAt: (arg0: number) => number; }) {
+    const encodedCPs = [];
+    for (let i = 0; i < str.length; i++)
+        encodedCPs.push(str.codePointAt(i) + 0xE0000);
+    return String.fromCodePoint(...encodedCPs);
+}
+
+function decode3y3(bio: string) {
+    const decoded3y3 = ["", "", ""];
+    const bioCPs = [...bio];
+    if (bioCPs.length > 0) {
+        let tempCPs = [];
+        let i = 0;
+        for (let j = 0; j < bioCPs.length; j++) {
+            const currCP = bioCPs[j].codePointAt(0) as number;
+            if (currCP === 0xE007E) {
+                if (tempCPs.length > 0)
+                    decoded3y3[i] = String.fromCodePoint(...tempCPs);
+                if (i > 1) break;
+                tempCPs = [];
+                i++;
+            } else if (0xE0000 < currCP && currCP < 0xE007E) {
+                tempCPs.push(currCP - 0xE0000);
+                if (j === bioCPs.length - 1)
+                    decoded3y3[i] = String.fromCodePoint(...tempCPs);
+            } else if (i > 0) {
+                if (tempCPs.length > 0)
+                    decoded3y3[i] = String.fromCodePoint(...tempCPs);
+                break;
+            }
+        }
+    }
+    return decoded3y3.join("");
+}
+
+export { decode3y3, encode3y3 };
+
