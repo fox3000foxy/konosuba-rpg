82cdb6629b91e25683c5f78183b13d3a1f8ee057
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-27 09:06:28 +0100

    refactor: update dev script to use chokidar for file watching and rebuild

---
+++ scripts/dev.js
@@ -0,0 +1,32 @@
+// scripts/dev.js
+import { spawn } from "child_process";
+import chokidar from "chokidar";
+import { build } from "esbuild";
+
+let server;
+
+function startServer() {
+  if (server) server.kill();
+  server = spawn("node", ["./dist/index.js"], { stdio: "inherit" });
+}
+
+async function buildAndStart() {
+  await build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    platform: "node",
+    target: "node18",
+    outfile: "./dist/index.js",
+    sourcemap: "inline",
+  });
+  startServer();
+}
+
+chokidar
+  .watch("src", { ignoreInitial: true, ignored: /dist|\.git/ })
+  .on("change", async (path) => {
+    console.log(`${path} changed, rebuilding...`);
+    await buildAndStart();
+  });
+
+buildAndStart();
