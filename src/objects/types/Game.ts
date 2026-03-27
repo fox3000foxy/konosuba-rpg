fffb10e87a5b7161360636e1e9aac7293053a0c9
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-27 14:55:39 +0100

    refactoring processGame.ts

--- src/objects/types/Game.ts
+++ src/objects/types/Game.ts
@@ -1,11 +1,11 @@
-import { Creature, MessagesTemplates } from "../../classes/Creature";
+import { Creature } from "../../classes/Creature";
 import { Team } from "../../classes/Player";
 import { GameState } from "../../enums/GameState";
 
 export type Game = {
   image?: Uint8Array;
   state: GameState;
-  messages: MessagesTemplates[] | string[];
+  messages: string[];
   embedDescription: string[];
   team: Team;
   creature: Creature;
