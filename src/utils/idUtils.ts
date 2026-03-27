a7e6d81cd3d242d2dd7cc327dad43265509b8508
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-27 15:12:07 +0100

    optimized utils

--- src/utils/idUtils.ts
+++ src/utils/idUtils.ts
@@ -2,37 +2,12 @@
 
 /** Generates a random ID of a given length */
 export function makeid(length: number): string {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZbcefijklmnopqrstuvwxyz0123456789";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZbcefijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result + "/";
+  return Array.from({ length }, () => characters.charAt(Math.floor(Math.random() * charactersLength))).join("") + "/";
 }
 
-/** Génère l'ID "Recommencer" en effaçant les lettres d'action */
+/** Simplifies the restartId logic using a regex */
 export function restartId(payload: string): string {
-  return payload
-    .split("/")
-    .join("")
-    .split("train")
-    .join("trqin")
-    .split("a")
-    .join("")
-    .split("trqin")
-    .join("train")
-    .split("d")
-    .join("")
-    .split("g")
-    .join("")
-    .split("h")
-    .join("")
-    .split("s")
-    .join("")
-    .split("p")
-    .join("");
+  return payload.replace(/[adhgsp]/g, "");
 }
--- src/utils/movesUtils.ts
+++ src/utils/movesUtils.ts
@@ -2,25 +2,11 @@
 
 /** Compresses a string of moves */
 export function compressMoves(moves: string): string {
-  let result = "";
-  let count = 1;
-
-  for (let i = 1; i <= moves.length; i++) {
-    if (moves[i] === moves[i - 1]) {
-      count++;
-    } else {
-      result += moves[i - 1] + (count > 1 ? count : "");
-      count = 1;
-    }
-  }
-
-  return result;
+  return moves.replace(/(.)\1+/g, (match, char) => `${char}${match.length}`);
 }
 
 /** Decompresses a compressed string of moves */
 export function decompressMoves(comp: string): string {
   // eslint-disable-next-line security/detect-unsafe-regex
-  return comp.replace(/([a-z])(\d+)?/g, (_, char, num) => {
-    return char.repeat(num ? parseInt(num) : 1);
-  });
+  return comp.replace(/([a-z])(\d+)?/g, (_, char, num) => char.repeat(num ? parseInt(num, 10) : 1));
 }
