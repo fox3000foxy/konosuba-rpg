f3f389900a758a1fb61e707584f355b7d1ed51fd
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-29 16:03:58 +0200

    added assets from peanuts-bot

---
+++ assets/characters-slots/list.js
@@ -0,0 +1,7 @@
+// const fs = require('fs')
+
+// console.log(JSON.stringify(fs.readdirSync("./")))
+
+console.log(JSON.stringify(
+	["adventurer.png","alderp.png","amy.png","aqua.png","arue.png","beldia.png","blacksmith.png","cecily.png","charlie.png","chomoske.png","chris.png","cielo.png","claire.png","daniel.png","darkness.png","epic_amy.png","epic_aqua.png","epic_arue.png","epic_cecily.png","epic_chris.png","epic_darkness.png","epic_erika.png","epic_iris.png","epic_megumin.png","epic_melissa.png","epic_mitsurigi.png","epic_rin.png","epic_yunyun.png","erika.png","eris.png","gold_chris.png","gold_megumin.png","gold_melissa.png","gold_yunyun.png","hans.png","iris.png","kazuma.png","komekko.png","lia.png","luna.png","megumin.png","melissa.png","mia.png","mitsurigi.png","random_women.png","random_young_female.png","rin.png","sena.png","sylvia.png","unknown.png","vanir.png","wiz.png","yunyun.png"].map(u=>u.split(".png")[0])
+))
