ed9497e13fc65ea843a1386d09e93941e4be65ad
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-04-03 14:23:29 +0200

    fixing leak and tests

--- src/render.ts
+++ src/render.ts
@@ -1,13 +1,10 @@
 import { config } from 'dotenv';
 import { Hono } from 'hono';
-import { registerApiRenderRoutes } from './routes/apiRender';
 import { calculateRPG } from './routes/rpg';
 
 config();
 
 const app = new Hono();
-
-registerApiRenderRoutes(app);
 app.get('/konosuba-rpg/:lang/*', calculateRPG);
 
 export default app;
