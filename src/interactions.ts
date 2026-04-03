c091a65f3870534de17ee92ebc6685972eb4a8cf
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-04-03 13:52:37 +0200

    splitting chage part 1

---
+++ src/interactions.ts
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+import { Hono } from 'hono';
+import { handleInteractions } from './routes/interactions';
+
+config();
+
+const app = new Hono();
+
+app.post('/api/interactions', handleInteractions);
+
+export default app;
