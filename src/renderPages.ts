d85fec57b03e2361747b1f5af9616c4e92d47a08
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-04-03 14:15:41 +0200

    render speedup by splitting warmup charges

---
+++ src/renderPages.ts
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+import { Hono } from 'hono';
+import { registerApiRenderRoutes } from './routes/apiRender';
+
+config();
+
+const app = new Hono();
+
+registerApiRenderRoutes(app);
+
+export default app;
